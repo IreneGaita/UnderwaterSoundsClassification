048fa47eb8275c6546289937424bad056316c0f8
Irene Gaita <i.gaita1@studenti.unisa.it>
2024-05-08 16:11:33 +0200

    fix bug Lettura_Freq.py

--- Set_Data/Lettura_Freq.py
+++ Set_Data/Lettura_Freq.py
@@ -1,5 +1,5 @@
-from pydub import AudioSegment
 import os
+from pydub import AudioSegment
 
 def get_audio_frequency(file_path):
     try:
@@ -23,30 +23,31 @@ def get_audio_frequency(file_path):
 
 def main():
     file_count = 0
+    exclude_files = ['.DS_Store', 'metadata-Target.csv']  # Lista di file da escludere
 
     # Ottieni il percorso assoluto del file corrente (dove viene eseguito il codice)
     current_file = os.path.abspath(__file__)
 
     # Ottieni il percorso della cartella genitore del file corrente
     parent_folder = os.path.dirname(current_file)
-
-    parent_folder = os.path.dirname(parent_folder)
+    parent_folder = os.path.dirname(parent_folder)  # Sali di un livello nella struttura delle directory
 
     # Definisci il percorso della cartella "Dataset"
     dataset_folder_path = os.path.join(parent_folder, "Dataset")
 
-    # Definisci il nome del file target
-    file_name = "Target"
+    # Percorso della cartella target all'interno di "Dataset"
+    path_main = os.path.join(dataset_folder_path, "Target")
 
-    # Unisci il percorso della cartella genitore con il nome del file target
-    path_main = os.path.join(dataset_folder_path, file_name)
+    # Scorri tutti i file nella cartella target
     for root, _, files in os.walk(path_main):
         for file_name in files:
-            file_path = os.path.join(root, file_name)
-            frequency = get_audio_frequency(file_path)
-            if frequency != 0:
-                print("Frequenza:", frequency)
-                file_count += 1
+            if file_name not in exclude_files:  # Verifica se il file deve essere escluso
+                file_path = os.path.join(root, file_name)
+                frequency = get_audio_frequency(file_path)
+                if frequency != 0:
+                    print("Frequenza:", frequency)
+                    file_count += 1
+
     print("Frequenze audio lette con successo.")
     print("Totale file audio:", file_count)
 
