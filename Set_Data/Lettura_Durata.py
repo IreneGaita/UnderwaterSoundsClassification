88b0e5e558c395742b3a88d04e0c274d0dd6e7e4
Biagio <biagioo2001.73@gmail.com>
2024-05-14 14:29:50 +0200

    fix

--- Set_Data/Lettura_Durata.py
+++ Set_Data/Lettura_Durata.py
@@ -1,55 +1,65 @@
 import os
 from pydub import AudioSegment
-import sys
-
-def count_files(directory, exclude_files):
-    # Conta i file totali escludendo quelli nella lista exclude_files.
-    return sum(1 for root, _, files in os.walk(directory) for file in files if file not in exclude_files)
 
 def get_audio_info(file_path):
-    # Ottiene informazioni sulla durata di un file audio e restituisce una tupla (minuti, secondi).
     try:
+        # Carica il file audio utilizzando pydub
         audio = AudioSegment.from_file(file_path)
+
+        # Ottieni la durata dell'audio in millisecondi
         duration_ms = len(audio)
-        return divmod(duration_ms // 1000, 60)
+
+        # Converti la durata da millisecondi a minuti e secondi
+        duration_min = duration_ms // 1000 // 60
+        duration_sec = duration_ms // 1000 % 60
+
+        return duration_min, duration_sec
     except Exception as e:
         print(f"Errore durante l'ottenimento delle informazioni audio per il file {file_path}: {e}")
         return None
 
 def audio_info():
-    exclude_files = {'.DS_Store', 'metadata-Target.csv', 'metadata-NonTarget.csv'}
-    current_file = os.path.abspath(__file__)
-    dataset_folder_path = os.path.join(os.path.dirname(os.path.dirname(current_file)), "Dataset")
-    subfolders = ["Non-Target"]
-
-    total_files = sum(count_files(os.path.join(dataset_folder_path, subfolder), exclude_files) for subfolder in subfolders)
     file_count = 0
-    audio_info_dict = {}
+    audio_info_dict = {}  # Dizionario per tenere traccia delle informazioni audio per ogni file
+    exclude_files = ['.DS_Store', 'metadata-Target.csv']  # Lista di file da escludere
 
+    # Ottieni il percorso assoluto del file corrente (dove viene eseguito il codice)
+    current_file = os.path.abspath(__file__)
+
+    # Ottieni il percorso della cartella genitore del file corrente
+    parent_folder = os.path.dirname(current_file)
+    parent_folder = os.path.dirname(parent_folder)  # Sali di un livello nella struttura delle directory
+
+    # Definisci il percorso della cartella "Dataset"
+    dataset_folder_path = os.path.join(parent_folder, "Dataset")
+
+    # Definisci le sottocartelle da visitare
+    subfolders = ["Non-Target", "Target"]
+
+    # Percorri tutte le sottocartelle
     for subfolder in subfolders:
+        # Percorso della sottocartella corrente
         path_main = os.path.join(dataset_folder_path, subfolder)
+
+        # Scorri tutti i file nella sottocartella corrente
         for root, _, files in os.walk(path_main):
             for file_name in files:
-                if file_name not in exclude_files:
+                if file_name not in exclude_files:  # Verifica se il file deve essere escluso
                     file_path = os.path.join(root, file_name)
                     duration = get_audio_info(file_path)
-                    if duration:
-                        if file_name not in audio_info_dict:
-                            audio_info_dict[file_name] = []
-                        audio_info_dict[file_name].append(duration)
+                    if duration is not None:
+                        audio_info_dict[file_name] = duration
                         file_count += 1
-                        sys.stdout.write(f"\rProgresso: {(file_count / total_files) * 100:.2f}%")
-                        sys.stdout.flush()
+                        print(f"File letto: {file_count} - Nome: {file_name}, Durata: {duration[0]} min {duration[1]} sec")
                     else:
                         print(f"Errore durante l'ottenimento delle informazioni audio per il file {file_path}")
 
-    sys.stdout.write('\n')  # Vai a capo una volta completato il processo
-    print("Informazioni audio lette con successo.\nTotale file audio:", file_count)
+    print("Informazioni audio lette con successo.")
+    print("Totale file audio:", file_count)
     return audio_info_dict
 
 if __name__ == "__main__":
     audio_info_dict = audio_info()
     print("Dizionario delle informazioni audio:")
-    for file_name, durations in audio_info_dict.items():
-        for duration in durations:
-            print(f"File: {file_name}, Durata: {duration[0]} min {duration[1]} sec")
+    for file_name, duration in audio_info_dict.items():
+        print(f"File: {file_name}, Durata: {duration[0]} minuti e {duration[1]} secondi")
