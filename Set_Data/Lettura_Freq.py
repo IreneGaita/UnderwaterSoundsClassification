6ada57dec91010e63c8d1a7f9fb2c63595070b77
Biagio <biagioo2001.73@gmail.com>
2024-05-20 11:51:05 +0200

    passaggio a librosa

--- Set_Data/Lettura_Freq.py
+++ Set_Data/Lettura_Freq.py
@@ -1,42 +1,45 @@
 import os
-from pydub import AudioSegment
 import sys
 import librosa
 import numpy as np
 from collections import defaultdict
+from pydub import AudioSegment
+
 
 def count_files(directory, exclude_files):
-    # Conta i file totali escludendo quelli nella lista exclude_files.
+    # Count total files excluding those in the exclude_files list.
     return sum(1 for root, _, files in os.walk(directory) for file in files if file not in exclude_files)
 
+
 def get_audio_frequency(file_path):
-    # Ottiene la frequenza di un file audio basato sull'estensione del file.
+    # Get the sampling rate of an audio file.
     try:
-        audio = AudioSegment.from_file(file_path)  # Funziona per tutti i formati supportati da pydub
+        audio = AudioSegment.from_file(file_path)  # Works for all formats supported by pydub
         return audio.frame_rate
     except Exception as e:
-        print(f"Errore durante la decodifica del file {file_path}: {e}")
+        print(f"Error decoding file {file_path}: {e}")
         return 0
 
+
 def find_max_frequency(file_path, high_pass_filter=False, cutoff_freq=100.0, amplitude_threshold=0.001):
-    # Carica l'audio con la frequenza di campionamento originale
+    # Load the audio with the original sampling rate
     y, sr = librosa.load(file_path, sr=None)
 
-    # Verifica se l'audio è silenzioso
+    # Check if the audio is silent
     if np.all(y == 0):
         return 0.0, sr
 
-    # Calcola lo spettrogramma con n_fft dinamico
+    # Compute the spectrogram with dynamic n_fft
     n_fft = min(len(y), 4096)
     S = np.abs(librosa.stft(y, n_fft=n_fft))
 
-    # Calcola lo spettro medio (media lungo il tempo)
+    # Compute the average spectrum (average along time)
     S_mean = np.mean(S, axis=1)
 
-    # Calcola le frequenze corrispondenti ai valori dello spettro
+    # Compute the frequencies corresponding to the spectrum values
     frequencies = np.linspace(0, sr / 2, len(S_mean))
 
-    # Trova la frequenza massima significativa (superiore alla soglia)
+    # Find the significant maximum frequency (above threshold)
     threshold = amplitude_threshold * np.max(S_mean)
     significant_indices = np.where(S_mean > threshold)[0]
 
@@ -47,47 +50,15 @@ def find_max_frequency(file_path, high_pass_filter=False, cutoff_freq=100.0, amp
 
     return round(max_frequency), sr
 
-def conteggio_frequenze_campionamento():
+
+def analyze_audio_files():
     exclude_files = {'.DS_Store', 'metadata-Target.csv', 'metadata-NonTarget.csv'}
     current_file = os.path.abspath(__file__)
     dataset_folder = os.path.join(os.path.dirname(os.path.dirname(current_file)), "Dataset")
     subfolders = ["Target", "Non-Target"]
 
-    sampling_frequency_counter = {}
-
-    total_files = sum(count_files(os.path.join(dataset_folder, subfolder), exclude_files) for subfolder in subfolders)
-    file_count = 0
-
-    for subfolder in subfolders:
-        path_main = os.path.join(dataset_folder, subfolder)
-        for root, _, files in os.walk(path_main):
-            for file_name in files:
-                if file_name in exclude_files:
-                    continue
-                file_path = os.path.join(root, file_name)
-                try:
-                    audio = AudioSegment.from_file(file_path)
-                    sampling_frequency = audio.frame_rate
-                    sampling_frequency_counter[sampling_frequency] = sampling_frequency_counter.get(sampling_frequency, 0) + 1
-                    file_count += 1
-                    sys.stdout.write(f"\rProgresso: {(file_count / total_files) * 100:.2f}%")
-                    sys.stdout.flush()
-                except Exception as e:
-                    print(f"Errore durante la decodifica del file {file_path}: {e}")
-
-    sys.stdout.write('\n')  # Vai a capo una volta completato il processo
-
-    return sampling_frequency_counter
-
-
-def conteggio_massime_frequenze_riproduzione():
-    exclude_files = {'.DS_Store', 'metadata-Target.csv', 'metadata-NonTarget.csv'}
-    current_file = os.path.abspath(__file__)
-    dataset_folder = os.path.join(os.path.dirname(os.path.dirname(current_file)), "Dataset")
-    subfolders = ["Target", "Non-Target"]
-
+    sampling_frequency_counter = defaultdict(int)
     max_frequency_counter = defaultdict(int)
-    max_frequencies = []
 
     total_files = sum(count_files(os.path.join(dataset_folder, subfolder), exclude_files) for subfolder in subfolders)
     file_count = 0
@@ -100,31 +71,38 @@ def conteggio_massime_frequenze_riproduzione():
                     continue
                 file_path = os.path.join(root, file_name)
                 try:
+                    # Get sampling frequency
+                    sampling_frequency = get_audio_frequency(file_path)
+                    if sampling_frequency > 0:
+                        sampling_frequency_counter[sampling_frequency] += 1
+
+                    # Get maximum frequency
                     max_frequency, sr = find_max_frequency(file_path, high_pass_filter=True, cutoff_freq=100.0,
                                                            amplitude_threshold=0.001)
-
                     if max_frequency > 0:
                         max_frequency_counter[max_frequency] += 1
-                        max_frequencies.append(max_frequency)
-
 
                     file_count += 1
-                    sys.stdout.write(f"\rProgresso: {(file_count / total_files) * 100:.2f}%")
+                    sys.stdout.write(f"\rProgress: {(file_count / total_files) * 100:.2f}%")
                     sys.stdout.flush()
                 except Exception as e:
-                    print(f"Errore durante la decodifica del file {file_path}: {e}")
+                    print(f"Error decoding file {file_path}: {e}")
 
-    sys.stdout.write('\n')  # Vai a capo una volta completato il processo
-    print(f"Totale file letti: {file_count}")
+    sys.stdout.write('\n')  # Move to the next line once the process is completed
 
-    return max_frequency_counter
+    return sampling_frequency_counter, max_frequency_counter
+
 
 if __name__ == "__main__":
-    # Eseguiamo i metodi per ottenere i dizionari con i conteggi delle frequenze
-    # sampling_frequency_counter = conteggio_frequenze_campionamento()
-    max_frequency_counter = conteggio_massime_frequenze_riproduzione()
+    # Execute the method to get the dictionary with frequency counts
+    sampling_frequency_counter, max_frequency_counter = analyze_audio_files()
 
-    # Stampiamo i risultati per le massime frequenze di riproduzione
-    print("\nMassime frequenze durante la riproduzione:")
+    # Print the results for sampling frequencies
+    print("\nSampling Frequencies:")
+    for frequency, count in sorted(sampling_frequency_counter.items()):
+        print(f"Sampling Frequency: {frequency} Hz, Number of files: {count}")
+
+    # Print the results for the maximum frequencies during playback
+    print("\nMaximum Frequencies during playback:")
     for frequency, count in sorted(max_frequency_counter.items()):
-        print(f"Frequenza massima: {frequency} Hz, Numero di file: {count}")
+        print(f"Maximum Frequency: {frequency} Hz, Number of files: {count}")
