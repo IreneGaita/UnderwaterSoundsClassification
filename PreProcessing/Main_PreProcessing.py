aa2698c3ca1d49da5b7e414a3fbf2dca18cd9f3b
Biagio <biagioo2001.73@gmail.com>
2024-05-23 14:59:06 +0200

    fix

--- PreProcessing/Main_PreProcessing.py
+++ PreProcessing/Main_PreProcessing.py
@@ -1,25 +1,50 @@
 import os
-from caricamento import process_audio_files
-from SetDurata import process_audio_files
+import sys
+from caricamento import conversione_audio
+from SetDurata import process_audio_files as segment_audio
+from Scalogrammi import processing_scalograms as process_scalograms_win
+from Scalogrammi_Mac import process_scalograms as process_scalograms_mac
+
 
 def main():
+    # Define dataset paths
     current_file = os.path.abspath(__file__)
-    dataset_folder_path = os.path.join(os.path.dirname(os.path.dirname(current_file)), "Dataset")
-    subfolders = ["Target", "Non-Target"]
-    subfolder_paths = [os.path.join(dataset_folder_path, subfolder) for subfolder in subfolders]
+    parent_folder = os.path.dirname(os.path.dirname(current_file))
+    dataset_folder_path = os.path.join(parent_folder, "Dataset")
+    new_dataset_folder_path = os.path.join(parent_folder, "NewDataset")
+    output_base_path = os.path.join(parent_folder, "Scalograms")
 
+    # Convert audio files
     print("Inizio PreProcessing")
     print("Conversione audio .mp3 a .wav")
     print("Cambio multi canale a mono")
-    print("set frequenza di campionamento a 86400 Hz")
-    process_audio_files(subfolder_paths)
+    print("Set frequenza di campionamento a 86400 Hz")
+    process_audio(dataset_folder_path)
+
+    # Segment and cut audio files
     print("Proseguo con segmentazione e taglio a 3 secondi")
-    current_file = os.path.abspath(__file__)
-    parent_folder = os.path.dirname(os.path.dirname(current_file))
-    input_dir = os.path.join(parent_folder, "Dataset")
-    output_dir = os.path.join(parent_folder, "NewDataset")
-    os.makedirs(output_dir, exist_ok=True)
-    segment_length = 3  # segment length in seconds
-    process_audio_files(input_dir, output_dir, segment_length)
+    segment_audio(dataset_folder_path, new_dataset_folder_path, segment_length=3)
+
+    # Create Scalograms
     print("Creazione Scalogrammi")
-    Scalogrammi()
+    create_scalograms(new_dataset_folder_path, output_base_path)
+
+
+def process_audio(dataset_folder):
+    subfolders = ["Target", "Non-Target"]
+    subfolder_paths = [os.path.join(dataset_folder, subfolder) for subfolder in subfolders]
+    conversione_audio(subfolder_paths)
+
+
+def create_scalograms(dataset_folder, output_base_path):
+    subfolders = ["Target", "Non-Target"]
+    subfolder_paths = [os.path.join(dataset_folder, subfolder) for subfolder in subfolders]
+
+    if sys.platform == "darwin":
+        process_scalograms_mac(subfolder_paths, output_base_path)
+    else:
+        process_scalograms_win(subfolder_paths, output_base_path)
+
+
+if __name__ == "__main__":
+    main()
