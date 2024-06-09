48888f833e775c954d747c06242bc6317d369e34
Biagio <biagioo2001.73@gmail.com>
2024-06-10 01:40:09 +0200

    BIG FIX

---
+++ Esperimento 1 Binary/AlexNet_Binary/Dataset.py
@@ -0,0 +1,32 @@
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+
+class CustomDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.classes = sorted([d for d in os.listdir(self.root_dir) if os.path.isdir(os.path.join(self.root_dir, d))])
+        self.data = []
+        self.targets = []
+
+        for idx, cls in enumerate(self.classes):
+            class_dir = os.path.join(self.root_dir, cls)
+            for file in os.listdir(class_dir):
+                file_path = os.path.join(class_dir, file)
+                if os.path.isfile(file_path):
+                    self.data.append(file_path)
+                    self.targets.append(idx)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_path = self.data[idx]
+        image = Image.open(img_path).convert('RGB')
+        target = self.targets[idx]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, target
